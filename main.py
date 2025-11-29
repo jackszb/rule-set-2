655873673b1213dbf5d659e30109415627b00285
jackszb <55304530+jackszb@users.noreply.github.com>
2025-11-29 18:17:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ adblock_urls = [
     "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt",
     "https://raw.githubusercontent.com/jackszb/mullvad-to-adguard/main/adguard/categories/adblock/adblock.txt",
     "https://raw.githubusercontent.com/jackszb/json-txt-2/main/domains.txt",
+    "https://raw.githubusercontent.com/jackszb/sing-box-abc/main/httpdns-cn.txt",
 ]
 
 # 输出文件名改为 adblock
